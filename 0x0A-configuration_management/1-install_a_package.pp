821b9448c7ab2128c3badddd4f575313ec8db156
BlightG <birhane.gulilat@gmail.com>
2023-01-10 22:09:13 +0300

    task 1 major simplification

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,20 +1,6 @@
 # this mainfest downloads flask using pip
 
-exec { 'apt-get update':
-  command => '/usr/bin/apt-get update',
-  before  => Exec[ 'python -m venv venv' ],
-}
-
-exec { 'python -m venv venv':
-  command => '/usr/bin/python3 -m venv venv',
-}
-
-exec { 'source venv/bin/activate':
-  source  => 'venv/bin/activate',
-  require => Exec[ 'python -m venv venv']
-}
-
-exec { 'pip3 install flask':
-  command => 'pip3 flask=2.1.0',
-  require => Exec[ 'source venv/bin/activate' ],
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => 'pip',
 }

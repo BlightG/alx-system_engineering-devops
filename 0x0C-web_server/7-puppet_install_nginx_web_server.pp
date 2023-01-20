1ec83e6505b2a7b42618b0bf81938e3e9ff7f94e
BlightG <birhane.gulilat@gmail.com>
2023-01-20 12:14:46 +0000

    task 7 more trials later

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -2,6 +2,7 @@
 
 package {'nginx':
   ensure => 'present',
+  before => File['/var/www/html/index'],
 }
 
 file {'/var/www/html/index':
@@ -12,13 +13,18 @@ file {'/var/www/html/index':
 file {'/usr/shar/nginx/html/custom_404':
   ensure  => 'present',
   content => "Ceci n'est pas une page\n",
+  before  => Exec['sed'],
+  require => File['/var/www/html/index'],
 }
 
 exec {'sed':
   path    => 'usr/bin/sed',
-  command => "sed -i '/listen 80 default_server/a \\tlocation /redirect_me { \n \t\treturn 301 https://www.youtube.com/watch?v=QH2-TGUlwu4' /etc/nginx/sites-enabled/default",
+  command => "sudo sed -i '/listen 80 default_server/a \\tlocation /redirect_me { \n \t\treturn 301 https://www.youtube.com/watch?v=QH2-TGUlwu4' /etc/nginx/sites-enabled/default; sudo sed -i '3-0i\\terror_page 404 /custom_404.html;\n\tlocation = /custom_404.html {\n\t\troot /usr/share/nginx/html;\n\t\tinternal;\n\t}' /etc/nginx/sites-enabled/default",
+  notify  => Service['nginx'],
 }
-exec {'sed':
-  path    => 'usr/bin/sed',
-  command => "sed -i '3-0i\\terror_page 404 /custom_404.html;\n\tlocation = /custom_404.html {\n\t\troot /usr/share/nginx/html;\n\t\tinternal;\n\t}' /etc/nginx/sites-enabled/default"
+
+service {'nginx':
+  require => Exec['sed'],
+  ensure  => 'running',
+  enable  => true,
 }

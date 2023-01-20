c388b80a9193a2085227a5e5fad87a80a0f87561
BlightG <birhane.gulilat@gmail.com>
2023-01-20 15:41:14 +0300

    more modification

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,5 +1,5 @@
 # installing and configuring an Nginx server
-exec {'sed':
+exec { 'install':
   provider => shell,
-  command  => "sudo apt-get -y update;sudo apt-get -y install nginx;echo 'Hello World!' | sudo tee /var/www/html/index;sudo sed -i '/listen 80 default_server;/a \\tlocation /redirect_me { \n\t\treturn 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;\n\t}' /etc/nginx/sites-enabled/default; line='\terror_page 404 /custom_404.html;\n\tlocation = /custom_404.html {\n\t\troot /usr/share/nginx/html;\n\t\tinternal;\n\t}'; echo 'Ceci n'est pas une page\n' | sudo tee /usr/share/nginx/html/custom_404.html; sudo service nginx restart",
+  command  => 'sudo apt-get -y update; sudo apt-get -y install nginx; echo "Hello World!" | sudo tee /var/www/html/index.html; sudo sed -i "s+server_name _;+server_name _;\n\tlocation /redirect_me{\n\t\trewrite ^ https://www.youtube.com/watch?v=dQw4w9WgXcQ permanent;\n\t}+" /etc/nginx/sites-available/default; sudo service nginx start;',
 }

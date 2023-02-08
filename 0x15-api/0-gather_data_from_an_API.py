383a5fdad032c5ca68e39f645050d4837959f1b6
BlightG <birhane.gulilat@gmail.com>
2023-02-08 15:18:48 +0300

    added get method and reorganized module order

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 ''' script that returns employee ID and TO/DO list progress '''
-import json
 from sys import argv
+import json
 import urllib.request
 
 if __name__ == '__main__':
@@ -20,7 +20,7 @@ if __name__ == '__main__':
     with urllib.request.urlopen('https://jsonplaceholder.typicode.com' +
                                 f'/users/{argv[1]}') as req:
         list_response = parse_response(req.read())
-        user = list_response["name"]
+        user = list_response.get("name")
 
     with urllib.request.urlopen('https://jsonplaceholder.typicode.com' +
                                 f'/todos?userId={argv[1]}') as req:
@@ -28,7 +28,7 @@ if __name__ == '__main__':
         for dct in list_response:
             if dct['completed'] is True:
                 task_completed = task_completed + 1
-                title_list.append(dct['title'])
+                title_list.append(dct.get('title'))
         print(f'Employee {user} is done with tasks' +
               f' ({task_completed}/{len(list_response)}):')
         for title in title_list:
